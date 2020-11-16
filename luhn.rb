2246e3cf77a5dd61aab49e39b9c8976553c34271
Kareha Agesa <kmaagesa@gmail.com>
2020-11-15 18:14:41 -0800

    my guesses at time/space complexity

--- luhn.rb
+++ luhn.rb
@@ -1,27 +1,21 @@
-=begin
-Write your code for the 'Luhn' exercise in this file. Make the tests in
-`luhn_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/luhn` directory.
-=end
-
 class Luhn
 
+  # Time: O(n)?
+  # Space: O(n)?
   def self.valid?(num)
     # validations
     num = num.gsub(/\s+/, "")
     return false if num.length <= 1
     return false if /[^0-9]+/.match(num)
 
-    array = num.split("").reverse.map{|s| s.to_i}
-    array.each_with_index do |num, index|
+    array = num.split("").reverse.map{|s| s.to_i}           # Time: O(n)? Space: O(n)
+    array.each_with_index do |num, index|                   # Time: O(n)
       if index.odd?
         val = num * 2
         if val > 9
           val -= 9
         end
-        array[index] = val
+        array[index] = val                                   # Time: O(1)?
       end
     end
 
